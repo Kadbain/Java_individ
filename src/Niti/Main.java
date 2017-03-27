a73ac5e0ba97f25e53245ab967738c33459493d8
Kadbain <ps1x0209@gmail.com>
2017-03-27 12:15:23 +0300

    C_04

--- src/Niti/Main.java
+++ src/Niti/Main.java
@@ -11,18 +11,5 @@ import java.util.HashSet;
 // For sentence = "Man bites dog", the output should bereverseSentence(sentence) = "dog bites Man".
 public class Main {
     public static void main(String args[]) {
-        String s = "Man bites dog";
-        System.out.println(reverseSentence(s));
     }
-
-    public static String reverseSentence(String sentence) {
-        String outPutText = "";
-        String[] arr = sentence.split(" ");
-        for (int i = arr.length - 1; i >= 0; i--) {
-            outPutText += arr[i] + " ";
-        }
-
-        return outPutText;
-    }
-
 }
