d918987d3f66d813e4a92f53dfb0c1cb696957de
Kadbain <ps1x0209@gmail.com>
2017-03-28 14:55:47 +0300

    C_07

--- src/codeFight/Main.java
+++ src/codeFight/Main.java
@@ -5,9 +5,24 @@ import java.util.stream.BaseStream;
 /**
  * Created by Ps1X on 17.03.2017.
  */
-
+ class Oke implements Main.A.B {
+     interface C {
+         void stop();
+     }
+     public int goTo() {
+         System.out.println("uekznm");
+         return 5;
+     }
+}
 public class Main{
     public static void main(String[] args) {
-
+Oke o = new Oke();
+o.goTo();
+    }
+    interface A {
+        void go();
+        interface B{
+            int goTo();
+        }
     }
 }
---
+++ src/codeFight/reverseSentence/Main.java
@@ -0,0 +1,24 @@
+package codeFight.reverseSentence;
+
+/**
+ * Created by Ps1X on 24.03.2017.
+ */
+// Обратить порядок слов в заданном строковом предложении.
+// Вы можете предположить, что в предложении нет каких-либо ведущих, завершающих или повторяющихся пробелов.
+// For sentence = "Man bites dog", the output should bereverseSentence(sentence) = "dog bites Man".
+public class Main {
+    public static void main(String args[]) {
+        String s = "Man bites dog";
+        System.out.println(reverseSentence(s));
+    }
+
+    public static String reverseSentence(String sentence) {
+        String outPutText = "";
+        String[] arr = sentence.split(" ");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            outPutText += arr[i] + " ";
+        }
+
+        return outPutText.trim();
+    }
+}
