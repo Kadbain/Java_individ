b4b28f0842867707e4c64b6fa70af0e8af5ce106
Kadbain <ps1x0209@gmail.com>
2017-05-05 14:52:18 +0300

    C_28

---
+++ src/patterns/Main.java
@@ -0,0 +1,22 @@
+package patterns;
+
+import java.util.HashSet;
+
+/**
+ * Created by Ps1X on 04.05.2017.
+ */
+public class Main {
+
+    class A {
+        void f() {
+            System.out.println("f()");
+            /*
+            *
+            *
+            *
+            *
+            *
+             */
+        }
+    }
+}
