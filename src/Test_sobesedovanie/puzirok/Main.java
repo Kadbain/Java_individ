bbf2a2692c0a703395185753ba0bdd58f42adb3f
Kadbain <ps1x0209@gmail.com>
2017-05-09 11:02:27 +0300

    C_32

---
+++ src/Test_sobesedovanie/puzirok/Main.java
@@ -0,0 +1,27 @@
+package Test_sobesedovanie.puzirok;
+
+/**
+ * Created by Ps1X on 08.05.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = {3, 5, 1, 12, 7};
+        puzirok(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+
+    }
+    public static void puzirok(int [] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j+1] > arr [j]) {
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+
+                }
+            }
+        }
+    }
+}
