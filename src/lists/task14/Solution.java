41be4e596ab2baa4313e6726073d9cf806444458
Kadbain <ps1x0209@gmail.com>
2017-06-15 11:45:17 +0300

    C_113

--- src/lists/task14/Solution.java
+++ src/lists/task14/Solution.java
@@ -1,11 +1,9 @@
-package com.javarush.test.level07.lesson12.home04;
+package lists.task14;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /* Вводить с клавиатуры строки, пока пользователь не введёт строку 'end'
 Создать список строк.
