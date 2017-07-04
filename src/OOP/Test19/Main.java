71e170fd7d0f43b772d446478a921785cd53bc99
Kadbain <ps1x0209@gmail.com>
2017-07-04 15:51:25 +0300

    C_130

--- src/OOP/Test19/Main.java
+++ src/OOP/Test19/Main.java
@@ -8,7 +8,7 @@ package OOP.Test19;
 2. Создать классы WaterBridge и SuspensionBridge, которые реализуют интерфейс Bridge.
 3. Метод getCarsCount() должен возвращать любое захардкоженое значение типа int
 4. Метод getCarsCount() должен возвращать различные значения для различных классов
-5. В классе Solution создать публичный метод println(Bridge bridge).
+5. В классе Main создать публичный метод println(Bridge bridge).
 6. В методе println вывести на консоль значение getCarsCount() для объекта bridge.
 7. Каждый класс и интерфейс должны быть в отдельных файлах.
 */
